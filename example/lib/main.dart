5217a7510055ae6e177879daaef421e4fef82703
JaeHo Song <thruthesky@gmail.com>
2022-03-03 15:51:24 +0900

    update for ui

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:extended/extended.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_linkify/flutter_linkify.dart';
 
 GlobalKey<NavigatorState> globalNavigatorKey = GlobalKey();
 
